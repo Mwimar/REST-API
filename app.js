e1aa1a0bee72335612a12ddf9a777b8b20f34a5f
Mark Mwima <markmwimar@gmail.com>
2024-02-28 21:33:08 +0300

    json response

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/quote', function (req, res, next) {
+    res.json({
+        quote:'As you dive deeper into development, you become an expert'
+    });
+});
+
 app.listen('3000');
