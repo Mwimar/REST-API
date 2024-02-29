f399bcaf1233ec519aa91bcd91a646c3c2fc3de2
Mark Mwima <markmwimar@gmail.com>
2024-02-29 23:57:47 +0300

    getting quote

--- models/quote.model.js
+++ models/quote.model.js
@@ -1,7 +1,11 @@
 const db = require("../data/database");
 
 class Quote {
-  static async getRandomQuote() {}
+  static async getRandomQuote() {
+    const quotes = db.getDb.collection("quotes").find().toArray();
+    const randomQuote = Math.floor(Math.random() * quotes.length);
+    return randomQuote;
+  }
 }
 
 module.exports = Quote;
