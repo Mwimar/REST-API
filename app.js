18a2377e592ab8285c5f8716f1bbf87ddab0b106
Mark Mwima <markmwimar@gmail.com>
2024-03-01 00:10:39 +0300

    error handling database connection

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const quoteRoutes = require("./routes/quotes.route");
+const db = require("./data/database");
 
 const app = express();
 
 app.use("/quotes", quoteRoutes);
 
-app.listen("3000");
+db.initDb()
+  .then(function () {
+    app.listen("3000");
+  })
+  .catch(function (error) {
+    console.log("Error Connecting to database!");
+  });
